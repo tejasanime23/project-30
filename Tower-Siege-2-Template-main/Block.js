7f8f88a4d51b39dcd78014821719c9b9190f03c3
tejasanime23 <77193959+tejasanime23@users.noreply.github.com>
2021-05-22 10:31:13 +0530

    Add files via upload

---
+++ Tower-Siege-2-Template-main/Block.js
@@ -0,0 +1,37 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.image=loadImage("block.png");
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.Visibility=255;
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      //var pos= this.body.position;
+     // rectMode(CENTER);
+      //rect(pos.x,pos.y,this.width, this.height);
+      if(this.body.speed<3){
+        var angle=this.body.angle;
+        push()
+        translate(this.body.position.x,this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+      }else
+      {
+        World.remove(world,this.body);
+        push()
+        this.Visibility=this.Visibility-5;
+        tint(255,this.Visibility);
+        image(this.image,this.body.position.x,this.body.position.y,50,50);
+        pop();
+      }
+     
+    }
+}
